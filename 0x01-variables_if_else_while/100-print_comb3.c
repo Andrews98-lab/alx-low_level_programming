a20810f8198908f07413b22b9018a4bb8a390ac4
Andrews98-lab <84232691+Andrews98-lab@users.noreply.github.com>
2022-12-09 14:08:36 +0000

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
-  * main - Prints the numbers from 00 to 99
-  *
-  * Return: Always (Success)
-  */
+ * main - Entyr point
+ * Description: prints two digits combination
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	int c, i;
+        int c, i;
 
-	c = i = '0';
+        for (c = '0'; c <= '9'; c++)
+        {
+                for (i = '0'; i <= '9'; i++)
+                {
+                        if (c < i)
+                        {
+                                putchar(c);
+                                putchar(i);
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		for (i = '0'; i <= '9'; i++)
-		{
-			putchar(c);
-			putchar(i);
-
-			if ((c != '9') || (c == '9' && i != '9'))
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-
-	putchar('\n');
-
-	return (0);
+                                if (c != '8' || (c == '8' && i != '9'))
+                                {
+                                        putchar(',');
+                                        putchar(' ');
+                                }
+                        }
+                }
+        }
+        putchar('\n');
+        return (0);
 }
+  
