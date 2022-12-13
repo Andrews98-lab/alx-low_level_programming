8ca67b7218ccad244d6ef65d7f8f3269c2bedeec
Andrews <baidooandrews98@gmail.com>
2022-12-13 13:56:05 -0800

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,15 +6,10 @@
  * Return: returns 1 if `c` is lowercase
  *         otherwise always 0 (Success)
  */
-
 int _islower(int c)
   
 {
-  
-  if (c >= 97 && c <= 122)
-    
-    return (1);
-  
-  return (0);
-  
+       	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
 }
