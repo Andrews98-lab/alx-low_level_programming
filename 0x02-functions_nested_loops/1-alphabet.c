3647104d11201ac1d4bb884fabbc18158a76046b
Andrews <baidooandrews98@gmail.com>
2022-12-13 12:43:10 -0800

    aphalbet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,10 @@
  * print_alphabet - Entry point
  * Description a function to print alphabet
  * Return 0
- * */
+ */
 void print_alphabet(void)
 {
-	int ch;
-	
+	int ch;	
 	for (ch = 'a'; ch <= 'z'; ++ch)
 		_putchar(ch);
 	_putchar('\n');
