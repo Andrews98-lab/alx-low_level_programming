fc9650da0d0078dc11066d5e2891db821acb9b29
Andrews98-lab <84232691+Andrews98-lab@users.noreply.github.com>
2023-01-10 00:11:19 +0000

    Cfiles

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -4,10 +4,10 @@
 * segment of s which consist only of bytes from accept
 *@s:segment targeted
 *@accept:reference bytes container
+*
 *Return:returns the number of bytes in the initial
 * segment of s which consist only of bytes from accept
 */
-
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int bytes = 0;
@@ -29,4 +29,3 @@ unsigned int _strspn(char *s, char *accept)
 	}
 	return (bytes);
 }
-
--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -3,8 +3,10 @@
 *_strpbrk - The _strpbrk() function locates the first
 * occurrence in the string s of any of the bytes in
 *the string accept
+*
 *@s: string where search is made
 *@accept: string where searched bytes are located
+*
 *Return:Returns a pointer to the byte in s that matches
 * one of the bytes in accept, or NULL if no such byte is found
 */
@@ -24,6 +26,4 @@ char *_strpbrk(char *s, char *accept)
 	}
 
 	return ('\0');
-
 }
-
