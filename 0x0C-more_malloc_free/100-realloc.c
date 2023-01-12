73810603cd2f15debb72ae4c4bc8ad63749c5c48
Andrews98-lab <84232691+Andrews98-lab@users.noreply.github.com>
2023-01-12 19:47:25 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,9 +1,12 @@
 #include "main.h"
+
+
 /**
 * _realloc - reallocates a memory block
 * @ptr: pointer to the memory previously allocated with a call to malloc
 * @old_size: size of ptr
 * @new_size: size of the new memory to be allocated
+*
 * Return: pointer to the address of the new memory block
 */
 
