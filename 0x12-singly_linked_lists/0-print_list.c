75da9f6ef298192540a945cc661ac20da3bcc802
Andrews <baidooandrews98@gmail.com>
2023-01-26 12:25:58 -0800

    error fixed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,12 +2,12 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
 /**
  * print_list - check the code for Holberton School students.
  * @h: name of the list
  * Return: the number of nodes.
- */
+*/
+
 size_t print_list(const list_t *h)
 {
 	int count = 0;
