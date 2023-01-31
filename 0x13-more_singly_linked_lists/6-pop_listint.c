b97abc43667762630cad9c31692ec660575768ab
Andrews <baidooandrews98@gmail.com>
2023-01-31 15:29:58 -0800

    head file

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 #include <stdlib.h>
-
 /**
  * pop_listint - deletes the head node
  * @head: a double pointer
  *
  *Return: empty
  */
+
 int pop_listint(listint_t **head)
 {
 	listint_t *start;
